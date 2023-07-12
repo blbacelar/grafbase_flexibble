d534a1e55be6e403cd241b4b8f901791ad4166a7
Bruno Bacelar <blbacelar@gmail.com>
2023-07-12 22:55:10 +0100

    removed some variables in order to debug

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,9 @@ type Props = {
   searchParams: SearchParams
 }
 
-export const dynamic = 'force-dynamic'
-export const dynamicParams = true
-export const revalidate = 0
+// export const dynamic = 'force-dynamic'
+// export const dynamicParams = true
+// export const revalidate = 0
 
 const Home = async ({ searchParams: { category, endCursor } }: Props) => {
 
