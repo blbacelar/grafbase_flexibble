ea36bda5ff0a2dd0094d3ae71c7a468a732aa754
Bruno Bacelar <blbacelar@gmail.com>
2023-07-12 23:23:03 +0100

    copied the files from the repo master

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,19 +1,19 @@
-'use client'
+"use client";
 
-import { categoryFilters } from "@/constants"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
+import { categoryFilters } from "@/constants";
 
 const Categories = () => {
-  const router = useRouter()
-  const pathName = usePathname()
-  const searchParams = useSearchParams()
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
 
-  const category = searchParams.get('category')
+  const category = searchParams.get("category");
 
-  const handleTags = (filter: string) => {
-    router.push(`${pathName}?category=${filter}`)
-  }
+  const handleTags = (item: string) => {
+    router.push(`${pathName}?category=${item}`);
+  };
 
   return (
     <div className="flexBetween w-full gap-5 flex-wrap">
@@ -23,15 +23,18 @@ const Categories = () => {
             key={filter}
             type="button"
             onClick={() => handleTags(filter)}
-            className={`${ category === filter ? 'bg-light-white-300' : 'font-normal'} px-4 py-3 rounded-lg capitalize whitespace-nowrap`}
+            className={`${
+              category === filter
+                ? "bg-light-white-300 font-medium"
+                : "font-normal"
+            } px-4 py-3 rounded-lg capitalize whitespace-nowrap`}
           >
             {filter}
           </button>
         ))}
       </ul>
-
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
